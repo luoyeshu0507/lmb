8bea1e93a76c44fafd031299309faca40ec25e23
lizhiwei <550822844@qq.com>
2020-04-09 22:56:33 +0800

    finish

--- src/components/Performance/index.jsx
+++ src/components/Performance/index.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import { SecurityScanOutlined } from '@ant-design/icons';
 import './style.scss';
 
 export default function() {
   return (
     <div className="performance">
-      performance
+      <div className="title">
+        <div className="label">业绩</div>
+        <div className="sub">
+          <div>
+            <span>总业绩</span>
+            <em>500,000,000 Q币</em>
+          </div>
+          <div>
+            <span>本月业绩</span>
+            <em>5,000,000 Q币</em>
+          </div>
+        </div>
+      </div>
+      <div className="perf-search-label">业绩查询</div>
+      <div className="perf-item">
+        <SecurityScanOutlined />
+        一区查询
+        <em>**多</em>
+      </div>
+      <div className="perf-item">
+        <SecurityScanOutlined />
+        二区查询
+        <em>**多</em>
+      </div>
     </div>
   );
 }
--- src/components/Trade/index.jsx
+++ src/components/Trade/index.jsx
@@ -1,10 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {
+  EyeOutlined,
+  EyeInvisibleOutlined,
+  TransactionOutlined
+} from '@ant-design/icons';
 import './style.scss';
 
-export default function() {
-  return (
-    <div className="trade">
-      trade
-    </div>
-  );
+export default class extends Component {
+  state = {
+    showMoney: true,
+    money: 50000000
+  };
+
+  render() {
+    return (
+      <div className="trade">
+        <div className="title">
+          <div className="label">
+            余额 <em>(Q币)</em>
+            <span onClick={ this.toggleShowMoney.bind(this) }>
+              { this.state.showMoney? <EyeOutlined />: <EyeInvisibleOutlined /> }
+            </span>
+          </div>
+          <div className="value">
+            { this.formatedMoney }
+          </div>
+        </div>
+        <div className="white-block">
+          <div className="underlined-title">交易</div>
+          <div className="money-control">
+            <div className="transfer">
+              <TransactionOutlined />转账
+            </div>
+            <div className="widthdraw">
+              <TransactionOutlined />提现
+            </div>
+          </div>
+        </div>
+        <div className="white-block">
+          <div className="underlined-title">查询</div>
+          <div className="trade-log">交易记录</div>
+        </div>
+      </div>
+    );
+  }
+
+  toggleShowMoney() {
+    this.setState({
+      showMoney: !this.state.showMoney
+    })
+  }
+
+  get formatedMoney() {
+    let reg = /(\d{1,3})(?=(\d{3})+(?:$|\D))/g;
+    let money = this.state.money || 0;
+    if (this.state.showMoney) {
+      return money.toString().replace(reg, "$1,");
+    } else {
+      return '******';
+    }
+  }
 }
